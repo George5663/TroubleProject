6de5921d6efb2bd18fb729d49050508f39559026
George5663 <georgeskyrme.work@gmail.com>
2022-07-05 20:42:12 +0800

    Implemented npm start

---
+++ Players.js
@@ -0,0 +1,34 @@
+//Posistion 0 on board is home
+const player1 = {
+    playerColour: Red,
+    tokenOnePosistion: 0,
+    tokenTwoPosistion: 0,
+    tokenThreePosistion: 0,
+    tokenFourPosistion: 0,
+};
+
+const player2 = {
+    playerColour: Green,
+    tokenOnePosistion: 0,
+    tokenTwoPosistion: 0,
+    tokenThreePosistion: 0,
+    tokenFourPosistion: 0,
+};
+
+const player3 = {
+    playerColour: Blue,
+    tokenOnePosistion: 0,
+    tokenTwoPosistion: 0,
+    tokenThreePosistion: 0,
+    tokenFourPosistion: 0,
+};
+
+const player4 = {
+    playerColour: Yellow,
+    tokenOnePosistion: 0,
+    tokenTwoPosistion: 0,
+    tokenThreePosistion: 0,
+    tokenFourPosistion: 0,
+};
+
+//Order of Play = Red -> Green -> Blue -> Yellow
